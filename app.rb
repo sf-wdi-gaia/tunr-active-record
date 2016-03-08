c8e99172a6c09930ed18392aae5a98e571cbc507
Jesse Shawl <jesse@jshawl.com>
2016-03-08 15:24:25 -0500

    Add Song Model association

--- app.rb
+++ app.rb
@@ -4,7 +4,8 @@ require "active_record" # the ORM
 require "pry" # for debugging
 
 require_relative "db/connection" # require the db connection file that connects us to PSQL, prior to loading models
-require_relative "models/artist" # require the Artist class definition that we defined in the models/student.rb file
+require_relative "models/artist" # require the Artist class definition that we defined in the models/artist.rb file
+require_relative "models/song" # require the Song class definition that we defined in the models/song.rb file
 
 # This will put us into a state of the pry REPL, in which we've established a connection
 # with the wdi database and have defined the Student Class as an
